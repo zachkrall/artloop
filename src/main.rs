b116da7c121d88751c107067e1bb7d87f6adb447
Zach Krall <2532937+zachkrall@users.noreply.github.com>
2019-07-13 19:54:16 -0400

    testing time conversion

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use std::time::{Duration, Instant};
-use std::thread::sleep;
+// use std::{env,fs,process};
+// use std::time::{Duration, Instant};
+// use std::thread::sleep;
+
+fn min_to_mil(value: f32) -> i32{
+    return (value * 60000.) as i32;
+}
 
 fn main() {
 
+    // let args: Vec<String> = env::args().collect();
+    let sleep_time:f32 = 10.0;
+
     println!("Hello, world!");
 
-    while true {
-        let ten_millis = Duration::from_millis(2000);
-        let now = Instant::now();
+    // for arg in args{
+    //     println!("arg: {:?}", arg);
+    // }
 
-        sleep(ten_millis);
+    println!("{:?} milliseconds", min_to_mil(sleep_time));
 
-        println!("{} seconds", now.elapsed().as_secs() );
-    }
+    // while true {
+    //     let sleep_time = Duration::from_millis(2000);
+    //     let now = Instant::now();
+    //
+    //     sleep(sleep_time);
+    //
+    //     println!("{} seconds", now.elapsed().as_secs() );
+    // }
 
 }
