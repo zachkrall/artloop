3c20e0d5a3597f3f85aea2501c77dee32112662a
Zach Krall <2532937+zachkrall@users.noreply.github.com>
2019-07-14 04:04:55 -0400

    first working test of iterating through folder contents

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-// use std::{env,fs,process};
-// use std::time::{Duration, Instant};
-// use std::thread::sleep;
+#[allow(dead_code)]
+use std::{fs,process};
+use std::time::{Duration};
+use std::thread::sleep;
+use std::ffi::OsStr;
 
-fn min_to_mil(value: f32) -> i32{
-    return (value * 60000.) as i32;
+fn min_to_mil(value: f32) -> u64{
+    return (value * 60000.) as u64;
 }
+
+fn get_list(action_dir_path: &str) -> Vec<String> {
+    fs::read_dir(action_dir_path)
+        .unwrap()
+        .map(|x| {
+            x.unwrap()
+                .path()
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+        })
+        .collect()
+}
+
+// TODO: function to delete ds_store files
 
 fn main() {
 
     // let args: Vec<String> = env::args().collect();
-    let sleep_time:f32 = 10.0;
+    let sleep_time = min_to_mil(0.1);
 
     println!("Hello, world!");
 
@@ -17,15 +36,54 @@ fn main() {
     //     println!("arg: {:?}", arg);
     // }
 
-    println!("{:?} milliseconds", min_to_mil(sleep_time));
+    let content_path = "./art-loop-content";
 
-    // while true {
-    //     let sleep_time = Duration::from_millis(2000);
-    //     let now = Instant::now();
-    //
-    //     sleep(sleep_time);
-    //
-    //     println!("{} seconds", now.elapsed().as_secs() );
+    // let mut v = Vec::new();
+    let v = get_list(content_path);
+
+    // let paths = fs::read_dir(&content_path).unwrap();
+    // for path in paths {
+    //     //println!("Name: {}", path.unwrap().path().display())
+    //     if let Ok(path) = path {
+    //         // Here, `entry` is a `DirEntry`.
+    //         let newpath = path.file_name().into_string().unwrap();
+    //         let newstem = &path.path().file_stem();
+    //         v.push( (newpath, newstem) );
+    //     }
     // }
+
+    // let mut iterpath = paths.cycle();
+
+    // let mut array = ["Hello","Goodbye","It's me"].iter().cycle();
+    let mut array = v.iter().cycle();
+
+    if v.len() > 0 {
+        loop {
+
+            // println!("{:?}",&array.next().unwrap());
+            // // println!("{:?}",paths.next().unwrap().path().file_stem().unwrap());
+            // sleep(Duration::from_millis(sleep_time));
+
+            // dbg!(&array.next().unwrap());
+
+            let file = &array.next().unwrap();
+
+            let  mypath = format!("./art-loop-content/{}.app/Contents/MacOS/{}", file, file );
+            let mut mycommand = process::Command::new(OsStr::new(&mypath));
+
+            if let Ok(mut child) = mycommand.spawn() {
+                println!("==process {} begin==", file);
+                sleep(Duration::from_millis( sleep_time ));
+                child.kill().expect("command wasn't running");
+                println!("==process ended==");
+                sleep(Duration::from_millis(100));
+            } else {
+                println!("process unable to start");
+            }
+
+        }
+    } else {
+        println!("Folder is empty");
+    }
 
 }
