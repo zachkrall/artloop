8be876a59eb6e83d3f712ec7cfc1f480b49eaa4b
Zach Krall <2532937+zachkrall@users.noreply.github.com>
2019-07-16 15:11:19 -0400

    cleaned get_sleep_time and loop

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate glob;
 extern crate clap;
 
 use std::{
-    process,
+    process::{exit,Command},
     time::Duration,
     thread::sleep,
     ffi::OsStr,
@@ -15,20 +15,29 @@ use clap::{App,Arg};
 mod tests {
     use super::*;
     #[test]
-    fn time_conversion() {
-        // Test for time conversion function
-        assert_eq!(min_to_mil(10.0), 600000);
+    fn sleep_value_passed_string(){
+        assert_eq!(
+            Duration::from_millis(120000),
+            get_sleep_time(Some("oops"), 2.)
+        );
     }
     #[test]
-    fn sleep_value_passed_string(){
-        // Incase clap returns a string from command line prompt
-        assert_eq!(2, get_sleep_time(Some("oops"), 2));
+    fn sleep_value_passed_number(){
+        assert_eq!(
+            Duration::from_millis(60000),
+            get_sleep_time(Some("1"), 30.)
+        );
+    }
+    #[test]
+    fn match_file_type(){
+        let myvar = match Some("String"){
+            Some(path) => path,
+            None => "Oops"
+        };
+        assert_eq!("String", myvar);
     }
 }
 
-fn min_to_mil(value: f32) -> u64{
-    return (value * 60000.) as u64;
-}
 
 fn get_glob_list(dir_path: &str) -> Vec<String>{
     let mut v:Vec<String> = vec![];
@@ -42,32 +51,27 @@ fn get_glob_list(dir_path: &str) -> Vec<String>{
     return v;
 }
 
-fn get_sleep_time(_input: Option<&str>, default_time: u64) -> u64{
-    let value;
-
-    if _input == None{
-        value = "0.1".parse::<f32>();
+fn get_sleep_time(value: Option<&str>, default_time: f32) -> Duration{
+    // This feels crazy
+    if value.is_some() && value.unwrap().parse::<f32>().is_ok() {
+        return Duration::from_millis((value.unwrap().parse::<f32>().unwrap() * 60000.) as u64);
     } else {
-        value = _input.unwrap().to_string().parse::<f32>();
-    }
-    if value.is_err() {
-        return default_time;
-    } else {
-        return min_to_mil( value.unwrap() );
+        return Duration::from_millis((default_time * 60000.) as u64);
     }
 }
 
-fn welcome_message(path: &String, time: &u64){
-    println!("art loop");
+fn welcome_message(path: &String, time: &Duration){
+    println!("🔄 artloop");
     println!("-----");
     println!("🎨 content path: {}", path);
-    println!("⏰ transition time: {}ms", time);
+    println!("⏰ transition time: {:?}ms", time);
+    println!("-----");
 }
 
 fn main() {
-
     let args = App::new("ARTLOOP")
                     .author("Zach Krall <zachkrall@newschool.edu>")
+                    .version("0.1.0")
                     .about("Utility to cycle through a folder of generative art projects.")
                     .arg(Arg::with_name("FOLDER")
                         .help("Selects the folder to use. (Defaults to current folder.)")
@@ -80,43 +84,37 @@ fn main() {
                         .takes_value(true))
                     .get_matches();
 
-    let sleep_time = get_sleep_time(args.value_of("time"), 10);
+    let default_time = 10.0;
+    let sleep_time = get_sleep_time(args.value_of("time"), default_time);
     let content_path = args.value_of("FOLDER").unwrap_or(".").to_string();
-
-    let v:Vec<String>;
+    let apps = get_glob_list(&content_path);
 
     welcome_message(&content_path, &sleep_time);
-    println!("-----");
 
-    v = get_glob_list(&content_path);
-
-    if v.len()<1 {
+    if apps.len() < 1 {
         println!("🔍 can't find any apps in {}\n", &content_path);
-        process::exit(0);
+        println!("please supply a path to a folder containing .app files\n");
+        exit(0);
+    } if apps.len() == 1 {
+        println!("🔍 found 1 app\n-----");
     } else {
-        println!("🔍 found {} apps:", v.len());
-        for app in &v {
-            println!("   * {}", app);
-        }
-        println!("-----");
+        println!("🔍 found {} apps\n!-----", apps.len());
     }
 
-    let mut array = v.iter().cycle();
+    println!("🛑 to stop artloop, press ctrl+c (^C)\n-----");
+
+    let mut array = apps.iter().cycle();
 
     loop {
-
-        println!("finding next artwork ...\n",);
-
-        let file   = &array.next().unwrap();
-        let mut mycommand = process::Command::new(OsStr::new(&file));
-
-        if let Ok(mut child) = mycommand.spawn() {
-            println!("started \"{}\" ...", file);
-            sleep(Duration::from_millis( sleep_time ));
-            child.kill().expect("command wasn't running");
-            println!("ended\n");
+        println!("searching...\n",);
+        if array.next().is_some() {
+            let file = OsStr::new(array.next().unwrap());
+            let mut mycommand = Command::new(file);
+            if let Ok(mut child) = mycommand.spawn() {
+                println!("... playing \"{:?}\" ...", file);
+                sleep(sleep_time);
+                child.kill().expect("command expected");
+            }
         }
-
     }
-
 }
