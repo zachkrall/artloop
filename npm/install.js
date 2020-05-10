bf64e46d0f63dd3611fb99b689a7b0c0e48c4e5e
Zach Krall <zach@zachkrall.com>
2020-05-10 16:36:47 -0400

    change install process

--- npm/install.js
+++ npm/install.js
@@ -1,2 +1,9 @@
-const getBinary = require('./getBinary');
-getBinary().install();
+let exec = require("child_process").exec;
+
+exec(
+  "curl -LSfs https://japaric.github.io/trust/install.sh | \
+sh -s -- --git zachkrall/artloop",
+  (error, stdout, stderr) => {
+    console.log(stderr);
+  }
+);
